d7ca6476c16e6382533237c71d65d38789a2a10f
leirensheng@163.com <leirensheng@163.com>
2018-02-10 15:12:43 +0800

    测试提交到本地

--- src/base/loading/loading.vue
+++ src/base/loading/loading.vue
@@ -2,6 +2,7 @@
   <div class="loading">
     <img width="24" height="24" src="./loading.gif">
     <p class="desc">{{title}}</p>
+    <p>我修改了部分东西了</p>
   </div>
 </template>
 <script type="text/ecmascript-6">
